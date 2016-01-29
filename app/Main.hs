85c21d924f0d85683f6a93c1895b169d5e35c4e8
Sean Lee <freshdried@gmail.com>
2016-01-29 12:48:06 -0500

    updated

--- app/Main.hs
+++ app/Main.hs
@@ -14,30 +14,54 @@ main :: IO ()
 
 main = do
   _ <- reacquire 0 (putStrLn "hello")
-  putStrLn "\n"
-  r <- runInterpreter test
-  case r of
-    Left err -> putStrLn $ "Oops..." ++ show err
-    Right () -> putStrLn "\n"
 
+  putStr "\ESC[2J"
 
-test :: Interpreter ()
-test = do
-  loadModules ["app/Main.hs"]
-  setTopLevelModules ["Main"]
-  setImports ["Prelude"]
-  say "Loading Main.hs"
-  xss <- lift $ fmap lines (readFile "test.hs")
+  xss <- fmap lines (readFile "test.hs")
   mapM_ rep xss
 
 
+run :: Interpreter a -> IO (Either InterpreterError a)
 
-rep:: String -> Interpreter ()
+run x = runInterpreter $ do
+  loadModules ["app/Main.hs"]
+  setTopLevelModules ["Main"]
+  setImports ["Prelude", "Text.Show.Pretty"]
+  x
+
+
+
+altEval :: String -> IO (Either InterpreterError (Maybe String, String))
+altEval str = do
+  result <- run $ evalAndTypeOf str
+  case result of
+    Left err -> do
+      result <- run $ coolTypeOf str
+      case result of
+        Left err -> return $ Left err
+        Right result -> return $ Right result
+    Right result -> return $ Right result
+  where
+    coolTypeOf str = do
+      typeof <- typeOf str
+      return (Nothing, typeof)
+    evalAndTypeOf str = do
+      output <- eval $ str
+      typeof <- typeOf str
+      return (Just output, typeof)
+
+
+rep :: String -> IO ()
 rep str = do
-  say $ "> " ++ str
-  result <- eval str
-  typeof <- typeOf str
-  say $ result ++ "\t\t"++ typeof ++ "\n"
+  result <- altEval str
+  case result of
+    Left _ -> return ()
+    Right (l, r) -> do
+      putStrLn $ "λ: " ++ str
+      case l of
+        Just l -> putStrLn l
+        Nothing -> return ()
+      putStrLn $ "" ++ r ++ "\n\n\n"
 
 
 
@@ -46,4 +70,4 @@ say = liftIO . putStrLn
 
 
 dup :: forall a. a -> [a]
-dup x = [x, x]
+dup x = [x, x, x]
